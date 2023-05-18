3f2c98b8e9aa532c0b2ecee52119cf2d7dc4cd3e
ENTNEAZ <langyuez@outlook.com>
2023-05-18 20:49:48 +0800

    support 1.19.2

--- src/main/java/xyz/nacldragron/playerhighlighter/mixin/ChatListener.java
+++ src/main/java/xyz/nacldragron/playerhighlighter/mixin/ChatListener.java
@@ -2,6 +2,7 @@ package xyz.nacldragron.playerhighlighter.mixin;
 
 
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.MessageIndicator;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,15 +14,17 @@ import xyz.nacldragron.playerhighlighter.Properties;
 
 @Mixin(ChatHud.class)
 public class ChatListener {
-    @Inject(method = "addMessage(Lnet/minecraft/text/Text;I)V", at = @At(value = "HEAD"), cancellable = false)
-    public void checkChat(Text message, int messageId, CallbackInfo ci) {
+    @Inject(method = "logChatMessage", at = @At(value = "HEAD"), cancellable = false)
+    public void checkChat(Text message, MessageIndicator indicator, CallbackInfo ci) {
         //check if the chat is asking my position
-        if (Properties.player != null && Util.checkValidAskingPosition(message.getString())) {
-            Properties.player.sendChatMessage(Util.getPositionString());
+        String messageStr = message.getString().replaceAll("\r", "\\\\r").replaceAll("\n", "\\\\n");
+        if (Properties.player != null && Util.checkValidAskingPosition(messageStr)) {
+            String s = Util.getPositionString();
+            Properties.player.sendChatMessage(s, Text.of(s));
         }
 
         //check if someone is @ me
-        if (Properties.player != null && Util.checkValidAtMe(message.getString())) {
+        if (Properties.player != null && Util.checkValidAtMe(messageStr)) {
             // show a small title in client
             Properties.player.sendMessage(Text.of("§e§lSomeone is @ you!"), true);
         }

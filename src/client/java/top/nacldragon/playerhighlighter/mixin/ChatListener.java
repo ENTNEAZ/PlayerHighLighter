54b19bd126b075ffc8685d80e8d74d8244800bb0
ENTNEAZ <langyuez@outlook.com>
2024-06-03 11:47:03 +0800

    fix ChatListener function signature issue

--- src/client/java/top/nacldragon/playerhighlighter/mixin/ChatListener.java
+++ src/client/java/top/nacldragon/playerhighlighter/mixin/ChatListener.java
@@ -2,7 +2,8 @@ package top.nacldragon.playerhighlighter.mixin;
 
 
 import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.client.gui.hud.MessageIndicator;
+import net.minecraft.client.gui.hud.ChatHudLine;
+
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,18 +16,23 @@ import top.nacldragon.playerhighlighter.Util;
 @Mixin(ChatHud.class)
 public class ChatListener {
     @Inject(method = "logChatMessage", at = @At(value = "HEAD"))
-    public void checkChat(Text message, MessageIndicator indicator, CallbackInfo ci) {
+    public void checkChat(ChatHudLine indicator, CallbackInfo ci) {
+        if(!Properties.isRespondingPosition() || Util.getPlayer() == null){
+            return;
+        }
+
         //check if the chat is asking my position
-        String messageStr = message.getString().replaceAll("\r", "\\\\r").replaceAll("\n", "\\\\n");
-        if (Properties.isRespondingPosition() && Util.getPlayer() != null && Util.checkValidAskingPosition(messageStr)) {
+        String messageStr = indicator.content().getString().replaceAll("\r", "\\\\r").replaceAll("\n", "\\\\n");
+        if(Util.checkValidAskingPosition(messageStr)){
             String s = Util.getPositionString();
             Util.getPlayer().networkHandler.sendChatMessage(s);
         }
 
         //check if someone is @ me
-        if (Properties.isRespondingAtMe() && Util.getPlayer() != null && Util.checkValidAtMe(messageStr)) {
+        if(Util.checkValidAtMe(messageStr)){
             // show a small title in client
-            Util.getPlayer().sendMessage(Text.of("§e§lSomeone is @ you!"), true);
+            Util.getPlayer().sendMessage(Text.of(Util.getUsername() + " §e§lSomeone is @ you!"), true);
         }
+
     }
 }
